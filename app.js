73649a7facfba6c85a61f7136dfe3df2d644a896
khangtoh <khang@picocandy.com>
2016-05-23 13:58:48 +0800

    FB Messenger webhook implementation

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var restify = require('restify');
-var builder = require('botbuilder');
-
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'aiabot  ', appSecret: '02d88019dbc74c7eae6c67503e484ce6' });
-bot.add('/', function (session) {
-    session.send('Hello World');
-});
 
 // Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url);
+var botapp = restify.createServer();
+
+
+botapp.get('/webhooks', function (req, res) {
+  if (req.query['hub.verify_token'] === 'aia_bot_verify_token') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
+
+botapp.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', botapp.name, botapp.url);
 });
