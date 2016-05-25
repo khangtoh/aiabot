6ddd305fea5a6663b40fed043febe0fd9c47e159
Khang Toh <khang@picocandy.com>
2016-05-25 11:36:10 +0800

    Add echo text back to sender

--- app.js
+++ app.js
@@ -27,8 +27,9 @@ app.post('/webhooks', function (req, res) {
 			event = req.body.entry[0].messaging[i];
 			sender = event.sender.id;
 			if (event.message && event.message.text) {
-		  	text = event.message.text;
-		  	console.log("webhooks[POST]: entry ",text);
+		  		text = event.message.text;
+		  		console.log("webhooks[POST]: entry ",text);
+		  		sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
 			}
 		}
 	}
@@ -41,3 +42,27 @@ app.post('/webhooks', function (req, res) {
 app.listen(process.env.port || 3978, function () {
     console.log('%s listening to %s', app.name, app.url);
 });
+
+
+var token = "<page_access_token>";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
