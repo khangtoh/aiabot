a266e58a3e0172fb95cae6a0d6e02f11cf7a9559
Khang Toh <khang@picocandy.com>
2016-05-25 10:39:15 +0800

    fixes for webhook POST

--- app.js
+++ app.js
@@ -19,16 +19,20 @@ console.log("webhooks: %s" , req.query['hub']['verify_token']);
 
 // POST
 app.post('/webhooks', function (req, res) {
-  messaging_events = req.body.entry[0].messaging;
-  for (i = 0; i < messaging_events.length; i++) {
-    event = req.body.entry[0].messaging[i];
-    sender = event.sender.id;
-    if (event.message && event.message.text) {
-      text = event.message.text;
-      console.log("webhooks[POST]: ",text);
-    }
-  }
-  res.sendStatus(200);
+
+	if (req.body && req.body.entry) {
+		messaging_events = req.body.entry[0].messaging;
+		for (i = 0; i < messaging_events.length; i++) {
+			event = req.body.entry[0].messaging[i];
+			sender = event.sender.id;
+			if (event.message && event.message.text) {
+		  	text = event.message.text;
+		  	console.log("webhooks[POST]: ",text);
+			}
+		}
+	}
+  
+  res.send(200);
   res.next();
 
 });
